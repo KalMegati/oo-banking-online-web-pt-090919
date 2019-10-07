9189c16a6760071ba9c3b41133ded6caa8d3eee7
Srikalyan Megati <kalmegati@gmail.com>
2019-10-07 18:42:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,13 @@
 class Transfer
   
-  def initialize
+  attr_reader :giver, :receiver, :amount
+  
+  def initialize(giver, receiver, amount)
+    @giver = giver
+    @receiver = receiver
+    @amount = amount
+  end
+  
+  
   
 end
