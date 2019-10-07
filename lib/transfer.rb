454333dbcbc794c0895aac1d69ab047d3b122b1d
Srikalyan Megati <kalmegati@gmail.com>
2019-10-07 18:44:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,13 +1,15 @@
 class Transfer
   
-  attr_reader :sender, :receiver, :amount
+  attr_reader :sender, :receiver, :amount, :status
   
-  def initialize(sender, receiver, amount)
+  def initialize(sender, receiver, amount, status = "pending")
     @giver = giver
     @receiver = receiver
     @amount = amount
   end
   
-  
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
   
 end
